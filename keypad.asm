14c8991599012ecac0cd4a461018786de8499edf
JammyL <jamie-leppard@outlook.com>
2020-02-08 15:29:05 +0000

    08/02/2020-15:29 Basic input  checking setup

--- keypad.asm
+++ keypad.asm
@@ -1,11 +1,14 @@
 	 #include "p18f87k22.inc"
-
+	 
 acs0	udata_acs   ; reserve data space in access ram
 	 
 angle	    res 1
 unit_count  res 1
 bits_on	    res 1
-bit_count   res 1
+bits_count  res 1
+bits_test   res 1
+   
+code
 	
 get_angle
     clrf    TRISD
@@ -16,15 +19,24 @@ get_angle
     
 check_input_angle
     movlw   0x08
-    movwf   bit_count
+    movwf   bits_count
     movf    PORTD, W
     BTFSC   W, 3
     bra	    check_input_angle
     BTFSC   W, 7
     bra     check_zero
 zero_passed
-    decf    bit_count
-    BTFSC   W,	bit_count 
+    movwf   bits_test
+    BTFSC   bits_test, 0
+    incf    bits_on
+    RRNCF   bits_test
+    decfsz  bits_count
+    bra	    zero_passed
+    BTFSC   bits_count, 2
+    bra	    check_input_angle
+    BTFSC   bits_count, 3
+    bra	    check_input_angle
+    return
     
     
     
@@ -34,9 +46,5 @@ check_zero
     bra	    zero_passed
     
     
-test_valid_angle
     
-    
-	
-    
- 
+    end
