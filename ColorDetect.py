11f47606b01242034d1f6c7dc43323694afac34a
JammyL <jamie-leppard@outlook.com>
2020-02-12 11:54:53 +0000

    12/02/2020-11:55-Switched to HSV

--- ColorDetect.py
+++ ColorDetect.py
@@ -4,27 +4,28 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 
 while True:
-	_, frame = cap.read()
-	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	lower_green = np.array([110, 110, 110])
-	upper_green = np.array([255, 255, 255])
+    lower_green = np.array([20, 110, 110])
+    upper_green = np.array([40, 255, 255])
 
-	green_mask = cv2.inRange(rgb, lower_green, upper_green)
+    green_mask = cv2.inRange(hsv, lower_green, upper_green)
 
-	(contours,_) = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    (contours,_) = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-	for contour in contours: 		
-		area = cv2.contourArea(contour)
+    for contour in contours:
+        area = cv2.contourArea(contour)
 
-		if(area > 20):
-			x,y,w,h = cv2.boundingRect(contour)
-			frame = cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),10)
+        if(area > 20):
+            x,y,w,h = cv2.boundingRect(contour)
+            frame = cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),10)
+            print(x, y)
 
-	cv2.imshow("tracking", frame)
+        cv2.imshow("tracking", frame)
 
-	if cv2.waitKey(10) & 0XFF == ord('q'):
-		break
+    if cv2.waitKey(10) & 0XFF == ord('q'):
+        break
 
 cv2.destroyAllWindows()
 cap.release()
